70210938cafaef1186a3925d7100f57966294a5c
Ketan Anil Muddalkar <ketanmuddalkar98@gmail.com>
2025-01-15 19:11:56 +0000

    learned about enums

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return { "message" : "Hello ketan!"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return { "item_id" : item_id }
+@app.get("/models/{model_name}")
+async def getmodel(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
